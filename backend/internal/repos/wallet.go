700cd659415c4e3d728dd7971b97de50436c87ba
fridalif <fridalif2@gmail.com>
2025-05-05 10:54:33 +0300

    Начато написание структуры с конфигом

--- backend/internal/repos/wallet.go
+++ backend/internal/repos/wallet.go
@@ -5,10 +5,15 @@ import (
 	"backend/pkg/wallet"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type WalletRepositoryI interface {
 	InitTable()
 	GetWallet(id uuid.UUID) (*wallet.Wallet, *customerror.CustomError)
 	UpdateWallet() *customerror.CustomError
+}
+
+type walletRepository struct {
+	Pool *pgxpool.Pool
 }
