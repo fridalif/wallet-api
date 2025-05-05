7025deec85b5c81b6caafa2f0cde35982e0fb832
fridalif <fridalif2@gmail.com>
2025-05-05 21:29:12 +0300

    Дописан хендлер для UpdateBalance

--- backend/internal/handlers/wallet.go
+++ backend/internal/handlers/wallet.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"backend/internal/services"
 	"backend/pkg/customerror"
+	"backend/pkg/requests"
 	"log"
 	"net/http"
 
@@ -73,5 +74,55 @@ func (walletHandler walletHandler) GetBalance(ctx *gin.Context) {
 }
 
 func (walletHandler walletHandler) UpdateBalance(ctx *gin.Context) {
-
+	var userRequest requests.UpdateBalanceRequest
+	err := ctx.ShouldBindJSON(&userRequest)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
+			"status": http.StatusBadRequest,
+			"body":   gin.H{},
+			"error":  "Wrong input",
+		})
+		return
+	}
+	err = walletHandler.WalletService.UpdateBalance(userRequest.WalletId, userRequest.OperationType, userRequest.Amount)
+	if err == customerror.ErrWrongAmount {
+		ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
+			"status": http.StatusBadRequest,
+			"body":   gin.H{},
+			"error":  "Amount cant be less than zero",
+		})
+		return
+	}
+	if err == customerror.ErrWrongOperation {
+		ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
+			"status": http.StatusBadRequest,
+			"body":   gin.H{},
+			"error":  "Operation must be DEPOSIT or WITHDRAW",
+		})
+		return
+	}
+	if err == pgx.ErrNoRows {
+		ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
+			"status": http.StatusNotFound,
+			"body":   gin.H{},
+			"error":  "Wallet not found",
+		})
+		return
+	}
+	if err != nil {
+		customError := err.(customerror.CustomError)
+		customError.AppendModule("UpdateBalance")
+		log.Printf("%s", customError.Error())
+		ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
+			"status": http.StatusInternalServerError,
+			"data":   gin.H{},
+			"error":  "Internal Server Error",
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"body":   gin.H{},
+		"error":  nil,
+	})
 }
