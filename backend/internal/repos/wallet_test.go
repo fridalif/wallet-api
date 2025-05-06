89156518c3cc9ec7d29b7107a980bbfb45b18354
fridalif <fridalif2@gmail.com>
2025-05-06 09:47:57 +0300

    написаны тесты для GetWallet

--- backend/internal/repos/wallet_test.go
+++ backend/internal/repos/wallet_test.go
@@ -2,10 +2,13 @@ package repos_test
 
 import (
 	"backend/internal/repos"
+	"backend/pkg/customerror"
+	"backend/pkg/wallet"
 	"context"
 	"errors"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/stretchr/testify/mock"
@@ -87,6 +90,83 @@ func TestWalletRepository_CreateTables(t *testing.T) {
 				assert.NoError(t, err)
 			}
 			mockPool.AssertExpectations(t)
+		})
+	}
+}
+
+type GetWalletTest struct {
+	Name          string
+	WalletId      uuid.UUID
+	WaitingWallet *wallet.Wallet
+	Mock          func(*MockPool, *MockRow)
+	WaitingError  error
+}
+
+func TestWalletRepository_GetWallet(t *testing.T) {
+	testUUID := uuid.New()
+	testWallet := &wallet.Wallet{
+		ID:     testUUID,
+		Amount: 1000,
+	}
+	getWalletTests := []GetWalletTest{
+		{
+			Name:          "Success Test",
+			WaitingError:  nil,
+			WalletId:      testUUID,
+			WaitingWallet: testWallet,
+			Mock: func(p *MockPool, r *MockRow) {
+				p.On("QueryRow", mock.Anything, "SELECT id, amount FROM wallet WHERE id = $1", []interface{}{testUUID}).Return(r)
+				r.On("Scan", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+					mockArgs := args.Get(0).([]interface{})
+					idPtr := mockArgs[0].(*uuid.UUID)
+					*idPtr = testWallet.ID
+					amountPtr := mockArgs[1].(*int64)
+					*amountPtr = testWallet.Amount
+				}).Return(nil)
+			},
+		},
+		{
+			Name:          "Not Found Test",
+			WaitingError:  pgx.ErrNoRows,
+			WalletId:      testUUID,
+			WaitingWallet: nil,
+			Mock: func(p *MockPool, r *MockRow) {
+				p.On("QueryRow", mock.Anything, mock.Anything, mock.Anything).Return(r)
+				r.On("Scan", mock.Anything, mock.Anything).Return(pgx.ErrNoRows)
+			},
+		},
+		{
+			Name:          "Other Error Test",
+			WaitingError:  customerror.NewError("walletRepo.GetWallet", "127.0.0.1:8080", errors.New("Other error").Error()),
+			WalletId:      testUUID,
+			WaitingWallet: nil,
+			Mock: func(p *MockPool, r *MockRow) {
+				p.On("QueryRow", mock.Anything, mock.Anything, mock.Anything).Return(r)
+				r.On("Scan", mock.Anything, mock.Anything).Return(errors.New("Other error"))
+			},
+		},
+	}
+	for _, test := range getWalletTests {
+		t.Run(test.Name, func(t *testing.T) {
+			mockPool := new(MockPool)
+			mockRow := new(MockRow)
+			test.Mock(mockPool, mockRow)
+			repo := &repos.WalletRepository{
+				Pool: mockPool,
+				Host: "127.0.0.1",
+				Port: "8080",
+			}
+			gettedWallet, err := repo.GetWallet(context.Background(), test.WalletId)
+			if err != nil {
+				assert.ErrorIs(t, err, test.WaitingError)
+				assert.Equal(t, gettedWallet, test.WaitingWallet)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, gettedWallet.ID, test.WaitingWallet.ID)
+				assert.Equal(t, gettedWallet.Amount, test.WaitingWallet.Amount)
+			}
+			mockPool.AssertExpectations(t)
+			mockRow.AssertExpectations(t)
 		})
 	}
 }
